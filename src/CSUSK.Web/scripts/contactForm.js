35b69b4bf6f743a5f15513591018a6f413aeff55
Paul Seal <prjseal@gmail.com>
2019-02-28 00:29:07 +0000

    - getting home page to work

---
+++ src/CSUSK.Web/scripts/contactForm.js
@@ -0,0 +1,18 @@
+﻿var contactForm = contactForm || {
+    init: function () {
+        this.listeners();
+    },
+    listeners: function () {
+        $(document).on('click', '.contact-submit', function (e) {
+            e.preventDefault();
+            var form = $('#contact-form');
+            form.submit();
+        })
+    },
+    showResult: function () {
+        $("#form-outer").hide('slow');
+        $("#form-result").show('slow');
+    }
+};
+
+contactForm.init();
---
+++ src/CSUSK.Web/scripts/navigationMenu.js
@@ -0,0 +1,9 @@
+﻿$('li.dropdown :first-child').not('.textonly').on('click', function () {
+    var $el = $(this).parent();
+    if ($el.hasClass('open')) {
+        var $a = $el.children('a.dropdown-toggle');
+        if ($a.length && $a.attr('href')) {
+            location.href = $a.attr('href');
+        }
+    }
+});
